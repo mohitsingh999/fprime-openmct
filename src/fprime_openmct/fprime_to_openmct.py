e7955af0a375ec589e38cbf290c420a956555526
Simone Morettini <s.morettini.cs@gmail.com>
2024-02-03 07:26:17 +0000

    Made it work for any type supported by Fprime

--- src/fprime_openmct/fprime_to_openmct.py
+++ src/fprime_openmct/fprime_to_openmct.py
@@ -4,6 +4,7 @@ from fprime_gds.common.pipeline.dictionaries import Dictionaries
 from fprime_gds.executables.cli import ParserBase, StandardPipelineParser
 import json
 import fprime_openmct
+import os
 
 from fprime.common.models.serialize.array_type import ArrayType
 from fprime.common.models.serialize.bool_type import BoolType
@@ -28,30 +29,23 @@ class TopologyAppDictionaryJSONifier():
     The Topology App Dictionary JSONifier class takes a python dictionary of F-Prime Telemetry Definitions and Converts it to the OpenMCT Definition
     It includes the following data members:
 
-    1. int_type_list -> List of F-Prime Int Types
-    2. float_type-list -> List of F-Prime Float Types
-    3. framework_list -> List of F-Prime Framework Strings
-    4. measurement_list -> List of F-Prime Telemetry Measurements
-    5. dict_enum -> F-Prime Dictionary for F-Prime Enum Definitions
-    6. dict_test -> F-Prime Dictionary for F-Prime Commands, Events, Telem Channels, and Parameters
-    7. channel_list -> List of F-Prime Telemetry Channel Definitions
-    8. openmct_telem_dict -> OpenMCT-formatted Dictionary of F-Prime Telemetry Channel Information
+    1. measurement_list -> List of F-Prime Telemetry Measurements
+    2. dict_enum -> F-Prime Dictionary for F-Prime Enum Definitions
+    3. openmct_telem_dict -> OpenMCT-formatted Dictionary of F-Prime Telemetry Channel Information
 
     """
     def __init__(self, xml_path='MPPTDeploymentTopologyAppDictionary.xml'):
         project_dictionary = Dictionaries()
         project_dictionary.load_dictionaries(xml_path, packet_spec=None)
         
-        self.dictionary_of_channel = {}
+        self.__dictionary_of_channel = {}
 
-        for key, value in project_dictionary.channel_id.items():
+        for _, value in project_dictionary.channel_id.items():
             channel_name = value.get_full_name().replace(".", "_")
 
             keys_to_access_values_and_type = []
-            self.create_access_list(
-                value.get_type_obj(), keys_to_access_values_and_type
-            )
-            self.dictionary_of_channel[channel_name] = keys_to_access_values_and_type
+            self.create_access_list(value.get_type_obj(), keys_to_access_values_and_type)
+            self.__dictionary_of_channel[channel_name] = keys_to_access_values_and_type
 
         self.__measurement_list = []
         self.__init_states = {}
@@ -60,19 +54,20 @@ class TopologyAppDictionaryJSONifier():
         self.loadEntries()
 
         self.__openmct_telem_dict = {}
-        self.__openmct_telem_dict['name'] = xml_path.replace('.xml', '')
+        self.__openmct_telem_dict['name'] = os.path.splitext(os.path.basename(xml_path))[0]
         self.__openmct_telem_dict['key'] = xml_path.replace('.xml', '')
         self.__openmct_telem_dict['measurements'] = self.__measurement_list
 
     # Load Telemetry Channel List and format it to be in the OpenMCT Dictionary Format
     def loadEntries(self):
 
-        for chanel_name, keys_to_access_values_and_type in self.dictionary_of_channel.items():
+        for chanel_name, channel_data in self.__dictionary_of_channel.items():
             
-            for channel_member in self.dictionary_of_channel[chanel_name]:
+            for channel_member in channel_data:
                 measurement_entry = {}
                 measurement_entry['values'] = [{}, {}]
                 keys = channel_member.get("keys", [])
+
                 measurement_entry['name'] = chanel_name+(('_'+'_'.join(map(str, keys))) if len(keys)>0 else "")
                 measurement_entry['key'] = chanel_name+(('_'+'_'.join(map(str, keys))) if len(keys)>0 else "")
 
@@ -102,7 +97,7 @@ class TopologyAppDictionaryJSONifier():
 
                 self.__measurement_list.append(measurement_entry)   
 
-        self.__init_states = {**self.__init_states}#, **self.__dict_xml._init_serializables}
+        self.__init_states = {**self.__init_states}
 
     #Write OpenMCT dictionary to a JSON file
     def writeOpenMCTJSON(self, fname, fpath=fprime_openmct.__file__.replace('__init__.py', 'javascript')):
@@ -115,11 +110,12 @@ class TopologyAppDictionaryJSONifier():
         with open(fpath + '/' + fname + ".json", "w") as outfile:
             outfile.write(initialstates_json)    
 
-    def create_access_list(self, ch_type, keysArr_types=[], keys_until_now=[]):
+    def create_access_list(self, ch_type, keysArr_types=[], keys_until_here=[]):
         """
         It recursively create a list of dictionaries where each dictionary contains for each primary type of a channel:
-        * a list of keys to access a primary type
-        ****
+        * a list of keys to access the primary type
+        * a opemct type
+        * for enums, a dictionary representing the possible enum values
         """
         channel_types = {
             F32Type: 'float',
@@ -137,23 +133,23 @@ class TopologyAppDictionaryJSONifier():
 
         if ch_type in channel_types:
             channel_type = channel_types[ch_type]
-            keysArr_types.append({"keys":keys_until_now, "type": channel_type})
+            keysArr_types.append({"keys":keys_until_here, "type": channel_type})
             return
         elif(issubclass(ch_type, ArrayType)):
             for index in  range(0, ch_type.LENGTH):
                 # Call recursively to deserialize the channel element
-                upgrade_key_list= (keys_until_now+[index]).copy()
+                upgrade_key_list= (keys_until_here+[index]).copy()
                 self.create_access_list(ch_type.MEMBER_TYPE, keysArr_types, upgrade_key_list)
         elif(issubclass(ch_type, EnumType)):
-            keysArr_types.append({"keys":keys_until_now, "type": "enum", "enum":ch_type.ENUM_DICT})
+            keysArr_types.append({"keys":keys_until_here, "type": "enum", "enum":ch_type.ENUM_DICT})
             return
         elif(issubclass(ch_type, StringType)):
-            keysArr_types.append({"keys":keys_until_now, "type": "text"})
+            keysArr_types.append({"keys":keys_until_here, "type": "string"})
             return
         elif(issubclass(ch_type, SerializableType)):
             for member_name, member_value, member_format, member_desc in ch_type.MEMBER_LIST:
                 # Call recursively to deserialize the channel element
-                upgrade_key_list= (keys_until_now+[member_name]).copy()
+                upgrade_key_list= (keys_until_here+[member_name]).copy()
                 self.create_access_list(member_value, keysArr_types, upgrade_key_list)
                 
         else:
